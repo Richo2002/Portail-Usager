8afff6a1c5b88222b155a8c01b3a85604b220e7f
Richo2002 <richo2002.44@gmail.com>
2022-10-20 16:11:17 +0100

    my third commit

--- database/migrations/2022_07_25_122339_create_categories_table.php
+++ database/migrations/2022_07_25_122339_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string("code", 15);
+            $table->string("code");
             $table->string("description");
             $table->timestamps();
         });
--- database/migrations/2022_07_25_122534_create_structures_table.php
+++ database/migrations/2022_07_25_122534_create_structures_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('structures', function (Blueprint $table) {
             $table->id();
-            $table->string("code", 15);
+            $table->string("code");
             $table->string("description");
             $table->foreignId('category_id')->constrained();
             $table->timestamps();
